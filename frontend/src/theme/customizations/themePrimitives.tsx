fc1481a1beef4c0e2d4594f8454f14b75104869c
Dylan Black <dylan.jacob.black@gmail.com>
2024-10-10 22:53:49 -0700

    get rid of dark mode

--- frontend/src/theme/customizations/themePrimitives.tsx
+++ frontend/src/theme/customizations/themePrimitives.tsx
@@ -148,61 +148,6 @@ export const colorSchemes = {
         "hsla(220, 30%, 5%, 0.07) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.07) 0px 8px 16px -5px",
     },
   },
-  dark: {
-    palette: {
-      primary: {
-        contrastText: brand[50],
-        light: brand[300],
-        main: brand[400],
-        dark: brand[700],
-      },
-      secondary: {
-        contrastText: gray[50],
-        light: gray[300],
-        main: gray[400],
-        dark: gray[700],
-      },
-      info: {
-        contrastText: brand[300],
-        light: brand[500],
-        main: brand[700],
-        dark: brand[900],
-      },
-      warning: {
-        light: orange[400],
-        main: orange[500],
-        dark: orange[700],
-      },
-      error: {
-        light: red[400],
-        main: red[500],
-        dark: red[700],
-      },
-      success: {
-        light: green[400],
-        main: green[500],
-        dark: green[700],
-      },
-      grey: {
-        ...gray,
-      },
-      divider: alpha(gray[700], 0.6),
-      background: {
-        default: gray[900],
-        paper: "hsl(220, 30%, 7%)",
-      },
-      text: {
-        primary: "hsl(0, 0%, 100%)",
-        secondary: gray[400],
-      },
-      action: {
-        hover: alpha(gray[600], 0.2),
-        selected: alpha(gray[600], 0.3),
-      },
-      baseShadow:
-        "hsla(220, 30%, 5%, 0.7) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.8) 0px 8px 16px -5px",
-    },
-  },
 };
 
 export const typography = {
